ab6f782cdec70e0f773d6baedcd4340bc461dcef
Marta <martasagun95@gmail.com>
2022-11-07 19:54:40 +0100

    created function which gets id_product, new_price and returns product with new price

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
-// 5) напишите функцию, которая получает id товара и возвращает его рейтинг
-
-const rate_product = async (id_product) => {
+// 6) напишите функцию, которая получает id товара и новую цену и обновляет цену данного товара
+const change_price = async (id_product, new_price) => {
     const response = await fetch(`https://fakestoreapi.com/products/${id_product}`);
     const product = await response.json();
-    
-    console.log(product.rating.rate);
+    console.log({...product, price:new_price});
 }
-rate_product(5);
+
+change_price(1, 1200);
