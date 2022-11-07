5af608f89da9cf6445774c47efc4ad75660951c0
Marta <martasagun95@gmail.com>
2022-11-07 12:39:03 +0100

    an array was created that contains all the titles of the products

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// 1) сформировать массив, который содержит все названия продуктов
+
+const get_products = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const products = await response.json();
+    // Solution method 1
+    // console.log(products.map(product => product.title));
+
+    // Solution method 2
+    const result = products.reduce((titles, {title}) => [...titles, title], [])
+    console.log(result)
+}
+get_products();
